99a588831f10e0f1d4c3656fd791541880faadd4
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2023-01-27 19:59:25 -0300

    feature: sidebar complete

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -2,22 +2,75 @@ import * as S from "./style";
 import { Category } from "@styled-icons/boxicons-regular/Category";
 import { Search } from "@styled-icons/material/Search";
 import { Home } from "@styled-icons/entypo/Home";
+import { BookInformation } from "@styled-icons/fluentui-system-filled/BookInformation";
+import { useState } from "react";
 
 export default function Footer() {
+  const [select, setSelect] = useState(2);
+
   return (
     <S.Footer>
-      <S.Icon>
-        <Category />
-        <p>Categorias</p>
-      </S.Icon>
-      <S.Icon>
-        <Home />
-        <p>Home</p>
-      </S.Icon>
-      <S.Icon>
-        <Search />
-        <p>Buscar</p>
-      </S.Icon>
+      {select == 2 ? (
+        <S.Icon className="select" onClick={() => setSelect(2)}>
+          <div>
+            <Home />
+            <p>Home</p>
+          </div>
+        </S.Icon>
+      ) : (
+        <S.Icon onClick={() => setSelect(2)}>
+          <div>
+            <Home />
+            <p>Home</p>
+          </div>
+        </S.Icon>
+      )}
+      {select == 1 ? (
+        <S.Icon className="select" onClick={() => setSelect(1)}>
+          <div>
+            <Category />
+            <p>Categorias</p>
+          </div>
+        </S.Icon>
+      ) : (
+        <S.Icon onClick={() => setSelect(1)}>
+          <div>
+            <Category />
+            <p>Categorias</p>
+          </div>
+        </S.Icon>
+      )}
+
+      {select == 3 ? (
+        <S.Icon className="select" onClick={() => setSelect(3)}>
+          <div>
+            <Search />
+            <p>Buscar</p>
+          </div>
+        </S.Icon>
+      ) : (
+        <S.Icon onClick={() => setSelect(3)}>
+          <div>
+            <Search />
+            <p>Buscar</p>
+          </div>
+        </S.Icon>
+      )}
+      {select == 4 ? (
+        <S.Icon className="select" onClick={() => setSelect(4)}>
+          <div>
+            <BookInformation />
+            <p>Sobre nós</p>
+          </div>
+        </S.Icon>
+      ) : (
+        <S.Icon onClick={() => setSelect(4)}>
+          <div>
+            <BookInformation />
+            <p>Sobre nós</p>
+          </div>
+        </S.Icon>
+      )}
     </S.Footer>
   );
 }
--- components/Footer/style.js
+++ components/Footer/style.js
@@ -1,19 +1,34 @@
 import styled from "styled-components";
 
 export const Footer = styled.div`
-  height: 60px;
+  height: 52px;
   width: 95%;
+  min-width: 330px;
   background-color: #fff;
   gap: 2em;
   max-width: 500px;
   display: flex;
   align-items: center;
   position: fixed;
+  padding: 0 3rem;
   justify-content: space-around;
   bottom: 5px;
   border-radius: 10px;
-  box-shadow: 0px 0px 19px 0px rgba(0,0,0,0.2);
 
+  @media(max-width: 360px) {
+    transform: scale(0.9);
+  }
+
+  .select {
+    margin-bottom: 2rem;
+    background-color: white;
+    border-radius: 10px;
+
+    div {
+      width: 55px;
+      height: 60px;
+    }
+  }
 
   svg {
     width: 30px;
@@ -28,15 +43,20 @@ export const Icon = styled.div`
   margin-top: 0.8rem;
   transition: 0.1s ease-in-out;
   cursor: pointer;
-  color: #f67d50;
+  color: #EF4E23;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 55px;
+    height: 60px;
+  }
 
   p {
     margin-top: -0.1rem;
     font-size: 8pt;
     font-weight: 400;
-  }
-
-  :hover {
-    margin-bottom: 1rem;
   }
 `;
