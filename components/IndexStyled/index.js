294514d3b679882d54041b8482215e2c2d5c8053
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2023-01-27 13:48:49 -0300

    feature: post display grid responsive

--- components/IndexStyled/index.js
+++ components/IndexStyled/index.js
@@ -1,8 +1,41 @@
 import styled from "styled-components";
 
-
 export const PostCardContainer = styled.div`
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-gap: 25px 50px;
-`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 25px 50px;
+  transition: 0.5s;
+
+  @media (max-width: 1060px) {
+    transform: scale(0.8);
+  }
+  @media (max-width: 850px) {
+    transform: scale(0.7);
+  }
+
+  @media (max-width: 750px) {
+    transform: scale(0.6);
+  }
+
+  @media (max-width: 650px) {
+    grid-template-columns: repeat(1, 1fr);
+    transform: scale(1);
+  }
+
+  @media (max-width: 650px) {
+    transform: scale(0.9);
+  }
+
+  @media (max-width: 500px) {
+    transform: scale(0.8);
+  }
+  @media (max-width: 450px) {
+    transform: scale(0.7);
+  }
+  @media (max-width: 400px) {
+    transform: scale(0.65);
+  }
+  @media (max-width: 350px) {
+    transform: scale(0.6);
+  }
+`;
