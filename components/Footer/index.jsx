e5b4b5ac26e982e1ff3d77584b122f8b03a5889e
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2023-01-28 14:47:44 -0300

    feature: footer side bar chande state false

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -7,7 +7,7 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function Footer({ select, setSelect }) {
-  const [showButton, setShowButton] = useState(true);
+  const [showButton, setShowButton] = useState(false);
 
   let lastScrollTop = 0;
 
