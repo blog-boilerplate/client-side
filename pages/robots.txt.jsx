5177d9db976418806d02858f64cd5755a2bbbb9b
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2023-02-09 00:37:47 -0300

    feature: robots txt add

--- pages/robots.txt.jsx
+++ pages/robots.txt.jsx
@@ -1,9 +1,14 @@
-import fs from "fs";
-import path from "path";
+import React from 'react'
+import fs from 'fs'
+import path from 'path'
 
 export function getServerSideProps({ res }) {
-  res.setHeader("Content-Type", "text/plain");
-  res.statusCode = 200;
-  res.end(fs.readFileSync(path.join(process.cwd(), "robots.txt"), "utf8"));
-  return { props: {} };
+  res.setHeader('Content-Type', 'text/plain')
+  res.statusCode = 200
+  res.end(fs.readFileSync(path.join(process.cwd(), 'robots.txt'), 'utf8'))
+  return { props: {} }
+}
+
+export default function Robots() {
+  return null
 }
