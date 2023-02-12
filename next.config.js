6aa80dcc54b3ddab6b58ada09a2ec27d9742725b
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2023-02-12 19:04:04 -0300

    feature: add limit kb index

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ module.exports = {
     compiler: {
       // Enables the styled-components SWC transform
       styledComponents: true
+    },
+    experimental: {
+      largePageDataBytes: 256 * 1024
     }
   }
--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const apolloClient = initializeApollo();
 
 export default function Home({ posts }) {
   const [select, setSelect] = useState(2);
-  const [displayCount, setDisplayCount] = useState(18);
+  const [displayCount, setDisplayCount] = useState(12);
 
   const handleLoadMore = () => {
     setDisplayCount(displayCount + 9);
@@ -21,28 +21,8 @@ export default function Home({ posts }) {
 
   return (
     <>
-       <Head>
+      <Head>
         <title>Life Nômade - Blog de viagem e turismo</title>
-        <link rel="icon" href="favicon.ico"></link>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-        <meta
-          name="description"
-          content="Viaje com a gente pelo mundo! Nossos relatos de viagem incluem dicas incríveis, belas fotos e experiências únicas. Junte-se a nós em uma jornada para explorar culturas, sabores e paisagens exóticas. Inspire-se e planeje sua próxima aventura."
-        />
-        <meta
-          property="og:title"
-          content="Life Nômade - Blog de viagem e turismo"
-        />
-        <meta
-          property="og:description"
-          content="Viaje com a gente pelo mundo! Nossos relatos de viagem incluem dicas incríveis, belas fotos e experiências únicas. Junte-se a nós em uma jornada para explorar culturas, sabores e paisagens exóticas. Inspire-se e planeje sua próxima aventura."
-        />
-        <meta property="og:image" content="https://i.imgur.com/tIte0sw.png" />
       </Head>
       <Logo />
       <S.Container>
@@ -65,7 +45,7 @@ export default function Home({ posts }) {
 export async function getStaticProps() {
   const GET_POSTS = gql`
     query GetAllPost {
-      posts(first: 10000) {
+      posts(first: 100) {
         nodes {
           title
           content
