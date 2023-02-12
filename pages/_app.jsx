a5f7e3a16294ea603ae573e8017e79a223643b3c
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2023-02-12 18:24:05 -0300

    feature: google analystic add to life nomade

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,14 +3,31 @@ import { useApollo } from "../utils/apollo";
 import { Toaster } from "react-hot-toast";
 import GlobalStyles from "../styles/global";
 import MyCookieConsent from "../components/MyCookieConsent";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import * as gtag from "../lib/gtag";
+import Analytics from "../components/Analytics";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   const client = useApollo(pageProps.initialApolloState);
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <ApolloProvider client={client}>
       <GlobalStyles />
       <Component {...pageProps} />
+      <Analytics />
       <MyCookieConsent />
       <Toaster position="bottom-center" reverseOrder={false} />
     </ApolloProvider>
