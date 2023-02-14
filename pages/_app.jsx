6f2504e6372278b97450a14bbd8cc18d412761a4
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2023-02-14 16:03:42 -0300

    feature: css adjusted with styled components

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,7 +4,9 @@ import { Toaster } from "react-hot-toast";
 import GlobalStyles from "../styles/global";
 import MyCookieConsent from "../components/MyCookieConsent";
 import { useEffect } from "react";
+import { ThemeProvider } from "styled-components";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 import * as gtag from "../lib/gtag";
 import Analytics from "../components/Analytics";
@@ -25,11 +27,40 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <ApolloProvider client={client}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-      <Analytics />
-      <MyCookieConsent />
-      <Toaster position="bottom-center" reverseOrder={false} />
+      {/* <ThemeProvider> */}
+        <Head>
+          <link rel="icon" href="favicon.ico"></link>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com"  crossOrigin="true" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
+            rel="stylesheet"
+          />
+          <meta
+            name="description"
+            content="Viaje com a gente pelo mundo! Nossos relatos de viagem incluem dicas incríveis, belas fotos e experiências únicas. Junte-se a nós em uma jornada para explorar culturas, sabores e paisagens exóticas. Inspire-se e planeje sua próxima aventura."
+          />
+          <meta
+            property="og:title"
+            content="Life Nômade - Blog de viagem e turismo"
+          />
+          <meta
+            name="p:domain_verify"
+            content="f4e739dcb17f8e979f52c26b8d3f7e7c"
+          />
+          <meta name="monetag" content="598c6c161fec152d1fd30a1c7c960f7f" />
+          <meta
+            property="og:description"
+            content="Viaje com a gente pelo mundo! Nossos relatos de viagem incluem dicas incríveis, belas fotos e experiências únicas. Junte-se a nós em uma jornada para explorar culturas, sabores e paisagens exóticas. Inspire-se e planeje sua próxima aventura."
+          />
+          <meta property="og:image" content="https://i.imgur.com/tIte0sw.png" />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+        <Analytics />
+        <MyCookieConsent />
+        <Toaster position="bottom-center" reverseOrder={false} />
+      {/* </ThemeProvider> */}
     </ApolloProvider>
   );
 }
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,39 +1,41 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="pt-br">
-      <Head>
-        <link rel="icon" href="favicon.ico"></link>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
-          rel="stylesheet"
-        />
-        <meta
-          name="description"
-          content="Viaje com a gente pelo mundo! Nossos relatos de viagem incluem dicas incríveis, belas fotos e experiências únicas. Junte-se a nós em uma jornada para explorar culturas, sabores e paisagens exóticas. Inspire-se e planeje sua próxima aventura."
-        />
-        <meta
-          property="og:title"
-          content="Life Nômade - Blog de viagem e turismo"
-        />
-        <meta
-          name="p:domain_verify"
-          content="f4e739dcb17f8e979f52c26b8d3f7e7c"
-        />
-        <meta name="monetag" content="598c6c161fec152d1fd30a1c7c960f7f" />
-        <meta
-          property="og:description"
-          content="Viaje com a gente pelo mundo! Nossos relatos de viagem incluem dicas incríveis, belas fotos e experiências únicas. Junte-se a nós em uma jornada para explorar culturas, sabores e paisagens exóticas. Inspire-se e planeje sua próxima aventura."
-        />
-        <meta property="og:image" content="https://i.imgur.com/tIte0sw.png" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
