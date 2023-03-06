083d0737afad3613127bc00682d0769740056d5a
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2023-03-06 13:12:38 -0300

    feature: image background none

--- components/PostCard/style.js
+++ components/PostCard/style.js
@@ -4,9 +4,26 @@ export const PostCard = styled.div`
   ${({ imgUrl }) => css`
     width: 330.4px;
     height: 185.5px;
-    background-color: black;
-    border-radius: 7px;
     position: relative;
+    background: #f6f7f8;
+    background-image: linear-gradient(
+      to right,
+      #f6f7f8 0%,
+      #e0e0e0 20%,
+      #f6f7f8 40%,
+      #f6f7f8 100%
+    );
+    background-size: 80rem 14rem;
+    animation: placeholderShimmer 1s linear infinite forwards;
+
+    @keyframes placeholderShimmer {
+      0% {
+        background-position: -40rem 0;
+      }
+      100% {
+        background-position: 40rem 0;
+      }
+    }
   `}
 `;
 
@@ -20,7 +37,7 @@ export const TitlePost = styled.h2`
   color: #ffffff;
   padding: 0.2rem 0.5rem;
   max-width: 90%;
-  background-color: #537AA6;
+  background-color: #537aa6;
 `;
 
 export const PostCardContainer = styled.div`
